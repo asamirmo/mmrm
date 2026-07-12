1b76f98a43cea789f014304c29f52fdbed2a9490
asamirmo <ahmedsamirmohamed70@gmail.com>
2026-07-12 17:04:42 +0200

    Urgent changes to test1

--- test1.R
+++ test1.R
@@ -4,3 +4,15 @@ cars <- mtcars
 
 cars_model <- lm(hp ~wt , data = cars)
 summary(cars_model)
+
+
+
+
+###### The new programmer will put his code here #####
+
+
+
+
+#######################
+
+coefs <- cars_model$coefficients

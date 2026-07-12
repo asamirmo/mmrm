25681383ce7a5db4f7e7b67544a85c0bc9d4cd58
asamirmo <ahmedsamirmohamed70@gmail.com>
2026-07-12 16:49:07 +0200

    Added a new file which is test3

--- test1.R
+++ test1.R
@@ -4,3 +4,9 @@ cars <- mtcars
 
 cars_model <- lm(hp ~wt , data = cars)
 summary(cars_model)
+
+resid <- cars_model$df.residual
+
+
+
+
---
+++ test3.R
@@ -0,0 +1,7 @@
+install.packages("faraway")
+
+aflatoxin <- faraway::aflatoxin
+
+total <- mean(aflatoxin$total)
+
+
